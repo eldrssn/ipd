c09bd17b0b363f0ef13ae521fbb352c64242079a
eldrssn <maxkalimullin@gmail.com>
2024-03-20 14:41:29 +0500

    cars along logo

--- js/moveCar.js
+++ js/moveCar.js
@@ -4,17 +4,20 @@ export function moveCar(loop) {
     .to('.car-wrapper', {
       opacity: 0,
       x: loop.direction() === 'right' ? '50%' : '-50%',
-      duration: 0.5,
+      y: loop.direction() === 'right' ? '-90%' : '90%',
+      duration: 0.4,
       ease: 'power1.inOut',
     })
     .to('.car-wrapper', {
       x: loop.direction() === 'right' ? '-50%' : '50%',
+      y: loop.direction() === 'right' ? '90%' : '-90%',
       duration: 0,
     })
     .to('.car-wrapper', {
       x: 0,
+      y: 0,
       opacity: 1,
-      duration: 0.3,
+      duration: 0.4,
     });
   return car;
 }
